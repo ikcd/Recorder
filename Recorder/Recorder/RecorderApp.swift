aac4ba893db340d1e8adbbec49297edbe07a0edc
Dinesh Kumar K C <dinesh.kc@dyooti.com>
2023-06-09 13:10:38 +0530

    bas3e project

---
+++ Recorder/Recorder/RecorderApp.swift
@@ -0,0 +1,20 @@
+//
+//  RecorderApp.swift
+//  Recorder
+//
+//  Created by KCD on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RecorderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
